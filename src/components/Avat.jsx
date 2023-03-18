3323999e042966f018b3a5e5e7296f7276ead028
Gra_Soft Henrique <88609043+Graciano1997@users.noreply.github.com>
2023-03-18 10:14:03 +0100

    Update the Avatar pic

--- src/components/Avat.jsx
+++ src/components/Avat.jsx
@@ -1,10 +1,10 @@
 
-import GRACIANO from '../components/GRA.jpg';
+import GracianoH from '../components/GracianoH.jpg';
 import './css/avatar.css'
 export default function Avat(props){
     return(
         <div className={props.class}>
-<img src={GRACIANO} alt="Graciano Henrique" />
+<img src={GracianoH} alt="Graciano Henrique" />
         </div>
     )
 }
